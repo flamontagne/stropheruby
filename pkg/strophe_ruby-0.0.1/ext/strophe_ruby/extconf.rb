073b76a9e007096fc72f3d2acf75f1f2d4753c7f
Frank <flamontagne@gmail.com>
2008-12-10 14:19:03 -0500

    test...

--- pkg/strophe_ruby-0.0.1/ext/strophe_ruby/extconf.rb
+++
@@ -1,7 +0,0 @@
-require 'mkmf'
-dir_config("expat")
-have_library("expat")
-dir_config("strophe")
-have_library("strophe")
-have_library("resolv")
-create_makefile("strophe_ruby")
--- pkg/strophe_ruby-0.0.1/test/test_strophe_ruby_extn.rb
+++
@@ -1,9 +0,0 @@
-require "test/unit"
-
-$:.unshift File.dirname(__FILE__) + "/../ext/strophe_ruby"
-
-class TestStropheRubyExtn < Test::Unit::TestCase
-  def test_truth
-    assert true
-  end
-end
